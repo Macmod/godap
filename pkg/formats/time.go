8dcc70aabd9953de067bcd96132fe6594f8013f6
Macmod <zz.mcmd@gmail.com>
2025-10-27 12:08:50 -0300

    Adding support for times 'in the future' in time formatting.

--- pkg/formats/time.go
+++ pkg/formats/time.go
@@ -6,27 +6,67 @@ import (
 )
 
 func GetTimeDistString(diff time.Duration) string {
+	if diff == 0 {
+		return "(0 seconds ago)"
+	}
+
+	future := diff < 0
+	if future {
+		diff = -diff
+	}
+
+	days := int(diff.Hours() / 24)
 	var distString string
 
-	daysAgo := int(diff.Hours() / 24)
-	if daysAgo == 0 {
-		hoursAgo := int(diff.Hours())
-		if hoursAgo == 0 {
-			minutesAgo := int(diff.Minutes())
-			if minutesAgo == 0 {
-				distString = fmt.Sprintf("(%d seconds ago)", int(diff.Seconds()))
-			} else if minutesAgo == 1 {
-				distString = "(1 minute ago)"
+	if days == 0 {
+		hours := int(diff.Hours())
+		if hours == 0 {
+			minutes := int(diff.Minutes())
+			if minutes == 0 {
+				seconds := int(diff.Seconds())
+				if future {
+					distString = fmt.Sprintf("(%d seconds from now)", seconds)
+				} else {
+					distString = fmt.Sprintf("(%d seconds ago)", seconds)
+				}
+			} else if minutes == 1 {
+				if future {
+					distString = "(1 minute from now)"
+				} else {
+					distString = "(1 minute ago)"
+				}
 			} else {
-				distString = fmt.Sprintf("(%d minutes ago)", minutesAgo)
+				if future {
+					distString = fmt.Sprintf("(%d minutes from now)", minutes)
+				} else {
+					distString = fmt.Sprintf("(%d minutes ago)", minutes)
+				}
+			}
+		} else if hours == 1 {
+			if future {
+				distString = "(1 hour from now)"
+			} else {
+				distString = "(1 hour ago)"
 			}
 		} else {
-			distString = fmt.Sprintf("(%d hours ago)", hoursAgo)
+			if future {
+				distString = fmt.Sprintf("(%d hours from now)", hours)
+			} else {
+				distString = fmt.Sprintf("(%d hours ago)", hours)
+			}
 		}
-	} else if daysAgo == 1 {
-		distString = "(yesterday)"
+	} else if days == 1 {
+		if future {
+			distString = "(tomorrow)"
+		} else {
+			distString = "(yesterday)"
+		}
 	} else {
-		distString = fmt.Sprintf("(%d days ago)", daysAgo)
+		if future {
+			distString = fmt.Sprintf("(%d days from now)", days)
+		} else {
+			distString = fmt.Sprintf("(%d days ago)", days)
+		}
 	}
 
 	return distString
